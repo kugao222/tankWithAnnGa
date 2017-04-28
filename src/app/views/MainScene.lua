cad7c1e27b9d7f73438353f25c5e99d7e3636345
youxiaolu <youxiaolu@hotmail.com>
2017-04-28 17:18:27 +0800

    xxxx

--- src/app/views/MainScene.lua
+++ src/app/views/MainScene.lua
@@ -6,7 +6,10 @@ local s = director:getWinSize() -- 960*640
 
 
 function MainScene:onCreate()
-	
+	local function update(dt)
+		
+	end
+	self:scheduleUpdateWithPriorityLua(update, -1)
 end
 --
 function MainScene:onEnter()
